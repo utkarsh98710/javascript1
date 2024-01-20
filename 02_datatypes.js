38c91b54acdbe78976a3f571786fdadd18fb1e8c
utkarsh89573 <utkarshgupta89573@gmail.com>
2024-01-20 23:55:56 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version (use new syntax)
+
+// alert( 3 + 3 ) we are using nodejs, not browser; it give error
+
+console.log(3
+    +3) //code readability should be high
+console.log("Utkarsh")
+
+// "use strict";
+// let public = 1500; cause error because public if future variable
+
+let name ="Utkarsh Gupta"
+let age = 18
+let isLoggeedIn = false
+let state //this is eg of undefined
+
+//total datatype = 2 to power 53
+//when number is way bigger than we use bigint datatype
+//string ==> ""
+//boolean ==> true/false
+//null ==> standalone value
+//undefined ==> abhi value assign nahi hui he
+//symbol ==> to find uniqueness 
+
+//non-primitive datatype
+//obeject
+
+console.log(typeof "Utkarsh");
+console.log(typeof age);
+console.log(typeof null); // it is obeject
+console.log(typeof undefined); //it is undefined
