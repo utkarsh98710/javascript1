0dca5ebe109797d83620cc58036957566cac7a17
utkarsh98710 <156845520+utkarsh98710@users.noreply.github.com>
2024-01-27 17:50:37 +0530

    Operation on datatypes

---
+++ 04_operationOnDatatypes.js
@@ -0,0 +1,38 @@
+let value = 3
+let negvalue =-(value)
+console.log(negvalue);
+
+// **Some more operations 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="Hello"
+let str2 =" Utkarsh"
+let str3 = str1 +str2
+console.log(str3);
+
+//string conversion or convert to primitive
+console.log("1"+2); //string
+console.log(1+"2"); //number
+console.log("1"+2+2); //string
+console.log(1+2+"2"); //number
+console.log(3+4*5%3); //dont use this type of code use correct syntax
+console.log((3+4)*5%3);
+
+//boolean types operation
+console.log(+true); // print 1
+//console.log(true+); // error
+console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = nums3 = 2+2 // less readiablity
+
+//increament operator
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
