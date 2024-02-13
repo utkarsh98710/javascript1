0830ec96d833e534e57cfe096de36197509782ef
utkarsh98710 <156845520+utkarsh98710@users.noreply.github.com>
2024-02-13 22:13:01 +0530

    Array continue

---
+++ 12_Arrays2.js
@@ -0,0 +1,39 @@
+const marvel_heros = ["Thor","Ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+//push existing array me push karta he, but concate new array return karta he .
+marvel_heros.push(dc_heros) // Array has been pushed in new array.
+console.log(marvel_heros); //nested array has been created.
+
+//Accesing of nested array given below
+console.log(marvel_heros[3][0]);
+// In marvel_heros array at index 3  new array is found and we acces element of new array by noraml way.
+
+//Another way to concate two array. concate method returns a new array.
+const allHeros = marvel_heros.concat(dc_heros) //nested array is created
+console.log(allHeros);
+
+//Another way to concate the array is **(Spread-method)**
+//This method breaks the element of array in single element.
+const all_new_heros =[...marvel_heros,...dc_heros]
+
+const another_array =[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+//how to handle above condition using [flatArr method]
+//Retruns a new array with all sub-array elements concatenatel 
+//into it recursively up to the specified depth.
+const real_another_array = another_array.flat(Infinity) //in this function we have to give depth(how many array we want) so we basically give infinity
+console.log(real_another_array);
+
+//How to check this is array or not.
+console.log(Array.isArray("Hitesh"));
+//convert Hitesh to array.
+console.log(Array.from("Hitesh"));
+
+//Interisting Case
+console.log(Array.from({name:"hitesh"})); //give empty array.because it is not able to convert keys to array.
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
