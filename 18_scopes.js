0bdffe4fd4255dbcf9b72c9fe75320471117308e
utkarsh89573 <utkarshgupta89573@gmail.com>
2024-03-07 22:16:30 +0530

    scope

--- 18_scopes.js
+++ 18_scopes.js
@@ -1,3 +1,4 @@
+//{} -> scope
 // let a=10
 // const b=20
 // var c=30
@@ -18,10 +19,10 @@ if (true){  // iske bhar jo bhi likha he vo global scope he.ye block scope he . 
     console.log("Inner: ",a); // inner a ki value 10 he outside 300 he.
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i]; // we want i ki value andar rahe bhar nahi aaye .use block scope.
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i]; // we want i ki value andar rahe bhar nahi aaye .use block scope.
     
-}
+// }
 // console.log(a); a is not defined before declare a global variable
 // console.log(b); b is not defined
 console.log(c);  //c is defined outside a scope this is it problem
